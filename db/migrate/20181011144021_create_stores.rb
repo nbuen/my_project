ea5a5685b1bb5a8325fcc34ffedefd8198b73f42
nikko <nikko.buen@yahoo.com>
2018-10-11 14:42:30 +0000

    created tables

---
+++ db/migrate/20181011144021_create_stores.rb
@@ -0,0 +1,12 @@
+class CreateStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :stores do |t|
+      t.string :name
+      t.string :address
+      t.decimal :latitude
+      t.decimal :longitude
+
+      t.timestamps
+    end
+  end
+end
