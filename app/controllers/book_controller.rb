4f18f3e01571a46fd593b99fe23280bcf6e7381a
nikko <nikko.buen@yahoo.com>
2018-10-12 04:28:22 +0000

    added review section for individual pages

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,5 +1,7 @@
 class BookController < ApplicationController
   def index
   	@book = Book.find(params[:id])
+  	book = @book.title
+  	@reviews = Review.where(["book_title = '#{book}'"])
   end
 end
